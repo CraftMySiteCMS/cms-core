cabaa47602c005826323967244811983cfcc2d31
LoGuardiaN <loguardian@hotmail.com>
2021-07-10 17:21:30 +0200

    [FIX] UTF-8

--- app/config/Manager.php
+++ app/config/Manager.php
@@ -11,7 +11,7 @@ class Manager {
         if(Manager::$db instanceof PDO) {
             return Manager::$db;
         } else {
-            Manager::$db = new \PDO("mysql:host=".getenv("DB_HOST"),getenv("DB_USERNAME"),getenv("DB_PASSWORD"));
+            Manager::$db = new \PDO("mysql:host=".getenv("DB_HOST"),getenv("DB_USERNAME"),getenv("DB_PASSWORD"), array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
             Manager::$db->query("CREATE DATABASE IF NOT EXISTS ".getenv("DB_NAME").";");
             Manager::$db->query("USE ".getenv("DB_NAME").";");
             return Manager::$db;
