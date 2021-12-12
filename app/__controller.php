29884dabcc3926afc990309ae0d5a0fea6b9d728
LoGuardiaN <loguardian@hotmail.com>
2021-12-12 20:00:26 +0100

    [FIX] Core required problem

--- app/__controller.php
+++ app/__controller.php
@@ -3,10 +3,14 @@
 $packageFolder = 'app/package/';
 $scanned_directory = array_diff(scandir($packageFolder), array('..', '.', '.model'));
 
+require("package/core/controllers/coreController.php");
+
 foreach ($scanned_directory as $package) :
-    $package_subfolder = "app/package/$package/controllers/";
-    $scanned_subdirectory = array_diff(scandir($package_subfolder), array('..', '.'));
-    foreach ($scanned_subdirectory as $controller) :
-        require("package/$package/controllers/$controller");
-    endforeach;
+    if($package != 'core') :
+        $package_subfolder = "app/package/$package/controllers/";
+        $scanned_subdirectory = array_diff(scandir($package_subfolder), array('..', '.'));
+        foreach ($scanned_subdirectory as $controller) :
+            require("package/$package/controllers/$controller");
+        endforeach;
+    endif;
 endforeach;
